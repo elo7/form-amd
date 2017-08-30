ba77ac8055e93d048e81befd7550b9e0191b2295
Luiz Corte Real <login+luiz@elo7.com>
2017-08-30 10:24:38 -0300

    Update dependencies and adjust test script

--- test.sh
+++ test.sh
@@ -1,3 +1,13 @@
 #!/bin/bash
 
-./node_modules/mocha-phantomjs/bin/mocha-phantomjs -R spec "http://localhost:8888/test/formTest.js.html";
+function killServer {
+	kill $NODE_PID
+}
+
+trap killServer EXIT
+
+./node_modules/http-server/bin/http-server -p 8888 &
+NODE_PID=$!
+sleep 3
+
+./node_modules/mocha-phantomjs/bin/mocha-phantomjs -p ./node_modules/.bin/phantomjs -R spec "http://localhost:8888/test/formTest.js.html";
